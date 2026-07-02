0f3920259f8e3f27ba5970eed84b2ff4070b7773
vlad1999777 <vlad1999777@gmail.com>
2026-07-02 12:56:09 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
+import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-TOKEN = "8817575346:AAF9JrALOF-x3iSh0pKiRU3Z86QXKaNXbfE"
-GROUP_ID = -1003777760609
+TOKEN = os.getenv("TOKEN")
+GROUP_ID = int(os.getenv("GROUP_ID"))
 
 async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.forward_message(
-        chat_id=GROUP_ID,
-        from_chat_id=update.message.chat_id,
-        message_id=update.message.message_id
-    )
+    if update.message:
+        await context.bot.forward_message(
+            chat_id=GROUP_ID,
+            from_chat_id=update.message.chat_id,
+            message_id=update.message.message_id
+        )
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, forward))
